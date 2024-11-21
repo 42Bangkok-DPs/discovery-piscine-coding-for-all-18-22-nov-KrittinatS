a2ead03cfdfe7f1f907615a3a2cbe5fbe88e846e
KrittinatS <67030270@kmitl.ac.th>
2024-11-21 07:41:50 +0000

    3.2

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,11 @@
+def main():
+    while True:
+        y = input("What you gotta say : ")
+
+        if y.strip().upper() == "STOP":
+            break
+        
+        print("I got that Anything else : ")
+
+if __name__ == "__main__":
+    main()
